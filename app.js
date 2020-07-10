14ab81fa30eca6e118a637d0d72efe14ef50e6d7
Aaron <aaronkh19@gmail.com>
2020-07-09 18:10:00 -0600

    setup connection, also need to figure out how to require the console.table

---
+++ app.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+// const console_table = require("console_table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Shotei1717!",
+  database: "employees_DB",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("You have an error connecting, check your code!" + err.stack);
+    return;
+  }
+  console.log("Connected as ID" + connection.threadId);
+});
